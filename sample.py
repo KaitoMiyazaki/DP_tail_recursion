3239572b2ced48151105d3d0ff7c2af739193ee2
kaito <immaginarybears@gmail.com>
2021-05-30 17:39:46 +0900

    Complete programing the Algorithm for TSP roughly

---
+++ sample.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+N = 20
+MAP_SIZE = 100
+
+np.random.seed(10)
+city_xy = np.random.rand(N, 2) * MAP_SIZE
+
+# plt.figure(figsize=(4,4))
+# plt.plot(city_xy[:,0], city_xy[:,1], 'o')
+# plt.show()
+
+class TSP:
+    def __init__(self, city):
+        self.city = city
+        self.order = list(np.random.permutation(city.shape[0]))
+        x = self.city[:,0]
+        y = self.city[:,1]
+        self.distance_matrix = np.sqrt((x[:,np.newaxis] - x[np.newaxis, :]) ** 2 + 
+                                    (y[:,np.newaxis] - y[np.newaxis, :]) ** 2)
+
+    def calculate_total_distace(self, order, distance_matrix):
+        """Calculate total distance traveld for given visit order"""
+        idx_from = np.array(order)
+        idx_to = np.array(order[1:] + [order[0]])
+        distance_arr = distance_matrix[idx_from, idx_to]
+
+        return np.sum(distance_arr)
+    
+    def visualize_visit_order(self, order, city_xy):
+        """Visualize traveling path for given visit order"""
+        route = np.array(order + [order[0]]) #add point of deperture
+        x_arr = city_xy[:,0][route]
+        y_arr = city_xy[:,1][route]
+
+        plt.figure(figsize=(4,4))
+        plt.plot(x_arr, y_arr, 'o-')
+        plt.show()
+
+    def calculate_2opt_exchange_cost(self, visit_order, i, j, distance_matrix):
+        """Calculate the difference of cost by applying given 2-opt exchange"""
+        n_cities = len(visit_order)
+        a,b = visit_order[i], visit_order[(i + 1) % n_cities]
+        c,d = visit_order[j], visit_order[(j + 1) % n_cities]
+
+        cost_before = distance_matrix[a,b] + distance_matrix[c,d]
+        cost_after = distance_matrix[a,c] + distance_matrix[b,d]
+
+        return cost_after - cost_before
+    
+    def apply_2opt_exchange(self, visit_order, i, j):
+        """Apply 2-opt exchanging on viit order"""
+        tmp = visit_order[i + 1 : j + 1]
+        tmp.reverse()
+        visit_order[i + 1 : j + 1] = tmp
+        return visit_order
+
+    def improve_with_2opt(self, visit_order, distance_matrix):
+        """Check all 2-opt neighbors and improve the visit order"""
+        n_cities = len(visit_order)
+        cost_diff_best = 0.0
+        i_best, j_best = None, None
+
+        for i in range(0, n_cities - 2):
+            for j in range(i + 2, n_cities):
+                if i == 0 and j == n_cities - 1:
+                    continue
+                cost_diff = self.calculate_2opt_exchange_cost(visit_order, i, j, distance_matrix)
+
+                if cost_diff < cost_diff_best:
+                    cost_diff_best = cost_diff
+                    i_best, j_best = i, j
+        
+        if cost_diff_best < 0.0:
+            visit_order_new = self.apply_2opt_exchange(visit_order, i_best, j_best)
+            return visit_order_new
+        else :
+            return None
+
+    def local_search(self, visit_order, distance_matrix, improve_func):
+        """Main procedure of local search"""
+        #cost_total = self.calculate_total_distace(visit_order, distance_matrix)
+
+        while True:
+            improved  = improve_func(visit_order, distance_matrix)
+            if not improved:
+                break
+
+            visit_order = improved
+
+        return visit_order
+
+    def solver(self):
+        self.visualize_visit_order(self.order, self.city)
+        total_distance = self.calculate_total_distace(self.order, self.distance_matrix)
+        print('初期解の総移動距離 = {}'.format(total_distance))
+
+        #近傍を計算
+        improved = self.local_search(self.order, self.distance_matrix, self.improve_with_2opt)
+        self.visualize_visit_order(improved, self.city)
+        total_distance = self.calculate_total_distace(improved, self.distance_matrix)
+        print('近傍探索適用後の総移動距離 = {}'.format(total_distance))
+def main():
+    my_tsp = TSP(city_xy)
+    order = list(np.random.permutation(N))
+    #my_tsp.visualize_visit_order(order, city_xy)
+    my_tsp.solver()
+if __name__ == "__main__":
+    main()
