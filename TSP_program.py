d57a829a745add34a48040a93273c68b0b005284
kaito <immaginarybears@gmail.com>
2021-06-02 16:27:54 +0900

    get all outputs

--- TSP_program.py
+++ TSP_program.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import os
 plt.style.use('ggplot')
 
 class TSP: #city(到達地点のx,y座標を持つ2次元のnp.array)
-    def __init__(self, city):
+    def __init__(self, city, num):
         self.city = city
         self.order = list(np.random.permutation(city.shape[0])) #初期フローをランダムに生成
+        self.num  = num
         x = self.city[:,0]
         y = self.city[:,1]
         self.distance_matrix = np.sqrt((x[:,np.newaxis] - x[np.newaxis, :]) ** 2 + 
@@ -20,7 +22,7 @@ class TSP: #city(到達地点のx,y座標を持つ2次元のnp.array)
 
         return np.sum(distance_arr)
     
-    def visualize_visit_order(self, order, city_xy):
+    def visualize_visit_order(self, order, city_xy, name):
         """Visualize traveling path for given visit order"""
         route = np.array(order + [order[0]]) #add point of deperture
         x_arr = city_xy[:,0][route]
@@ -28,6 +30,7 @@ class TSP: #city(到達地点のx,y座標を持つ2次元のnp.array)
 
         plt.figure(figsize=(6,6))
         plt.plot(x_arr, y_arr, 'o-')
+        plt.savefig(f'images/output_{self.num}/{name}.png')
         plt.show()
 
     def solve_with_greedy(self, city):
@@ -99,37 +102,51 @@ class TSP: #city(到達地点のx,y座標を持つ2次元のnp.array)
         return visit_order
 
     def solver(self):
+        my_directory = 'output_'+str(self.num)
+        if not os.path.exists(f'images/{my_directory}'):
+            os.makedirs(f'images/{my_directory}')
+        else:
+            pass
         #randomな解
-        self.visualize_visit_order(self.order, self.city)
-        total_distance = self.calculate_total_distace(self.order, self.distance_matrix)
-        print('random解の総移動距離 = {}'.format(total_distance))
+        with open(f'images/output_{self.num}/distance.txt', mode='w', encoding='utf-8') as f:
+            f.write(f'input_{self.num}.csv の出力結果\n')
+            self.visualize_visit_order(self.order, self.city, 'random')
+            total_distance = self.calculate_total_distace(self.order, self.distance_matrix)
+            f.write(f'random解の総移動距離 = {total_distance}\n')
+            #print('random解の総移動距離 = {}'.format(total_distance))
 
-        #greedy-method による解
-        start1 = time.time()
-        tour = self.solve_with_greedy(self.city)
-        elapsed_time1 = time.time() - start1
-        self.visualize_visit_order(tour, self.city)
-        total_distance = self.calculate_total_distace(tour, self.distance_matrix)
-        print(f'greedy法適用後の総移動距離 = {total_distance}')
-        print(f'実行時間：{elapsed_time1}')
+            #greedy-method による解
+            start1 = time.time()
+            tour = self.solve_with_greedy(self.city)
+            elapsed_time1 = time.time() - start1
+            self.visualize_visit_order(tour, self.city, 'greedy')
+            total_distance = self.calculate_total_distace(tour, self.distance_matrix)
+            f.write(f'greedy-methodの総移動距離 = {total_distance}\n')
+            #print(f'greedy法適用後の総移動距離 = {total_distance}')
+            f.write(f'実行時間：{elapsed_time1}\n')
+            #print(f'実行時間：{elapsed_time1}')
 
-        #初期解:random, 修正法：2-opt
-        start2 = time.time()
-        improved = self.local_search(self.order, self.distance_matrix, self.improve_with_2opt)
-        elapsed_time2 = time.time() - start2
-        self.visualize_visit_order(improved, self.city)
-        total_distance = self.calculate_total_distace(improved, self.distance_matrix)
-        print('近傍探索適用後の総移動距離 = {}'.format(total_distance))
-        print(f'実行時間：{elapsed_time2}')
+            #初期解:random, 修正法：2-opt
+            start2 = time.time()
+            improved = self.local_search(self.order, self.distance_matrix, self.improve_with_2opt)
+            elapsed_time2 = time.time() - start2
+            self.visualize_visit_order(improved, self.city, 'random+2opt')
+            total_distance = self.calculate_total_distace(improved, self.distance_matrix)
+            f.write(f'random+2-optの総移動距離 ={total_distance}\n')
+            #print('近傍探索適用後の総移動距離 = {}'.format(total_distance))
+            f.write(f'実行時間：{elapsed_time2}\n')
+            #print(f'実行時間：{elapsed_time2}')
 
-        #初期解：greedy-method, 修正法:2-opt
-        start3 = time.time()
-        improved = self.local_search(tour, self.distance_matrix, self.improve_with_2opt)
-        elapsed_time3 = time.time() - start3
-        self.visualize_visit_order(improved, self.city)
-        total_distance = self.calculate_total_distace(improved, self.distance_matrix)
-        print('greedy + 2-opt適用後の総移動距離 = {}'.format(total_distance))
-        print(f'実行時間：{elapsed_time3}')
+            #初期解：greedy-method, 修正法:2-opt
+            start3 = time.time()
+            improved = self.local_search(tour, self.distance_matrix, self.improve_with_2opt)
+            elapsed_time3 = time.time() - start3
+            self.visualize_visit_order(improved, self.city, 'greedy+2opt')
+            total_distance = self.calculate_total_distace(improved, self.distance_matrix)
+            f.write(f'greedy + 2-opt適用後の総移動距離 = {total_distance}\n')
+            #print('greedy + 2-opt適用後の総移動距離 = {}'.format(total_distance))
+            f.write(f'実行時間：{elapsed_time3}\n')
+            #print(f'実行時間：{elapsed_time3}')
 def read_input(filename):
     with open(filename) as f:
         cities = []
@@ -143,8 +160,9 @@ def main():
     # MAP_SIZE = 100
 
     # city_xy = np.random.rand(N, 2) * MAP_SIZE
-    city = np.array(read_input('city_data/input_5.csv'))
-    my_tsp = TSP(city)
+    number = 6
+    city = np.array(read_input(f'city_data/input_{number}.csv'))
+    my_tsp = TSP(city, number)
     my_tsp.solver()
 if __name__ == "__main__":
     main()
